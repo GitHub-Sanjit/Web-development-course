64317c0dfe06bb9c9e12619a6d7bfb0d296df91b
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-04-03 16:24:05 +0600

    MOdule 37

---
+++ MaileStone-7/module-37/fetch.js
@@ -0,0 +1,23 @@
+// 1. JSON
+// object to JSON   JSON.stringify()
+// JSON to object   JSON.parse()
+// 2. fetch
+fetch('url')
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+// keys, values
+const keys = Object.keys(student)
+const value = Object.values(student)
+
+// for
+array.forEach(element => {
+    
+});
+Map
+
+// for of
+// for in
+
+// add or remove from array
+// filter
